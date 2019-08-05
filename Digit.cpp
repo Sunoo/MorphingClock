98bb32c6354b5711ff29f0ca1111682c7a112720
David Maher <sunookitsune@kitsunet.org>
2019-08-04 22:22:23 -0400

    Abandoned plan to unflip everything and fully converted everything to use the 'flipped' methods.

--- Digit.cpp
+++ Digit.cpp
@@ -49,58 +49,43 @@ char Digit::Value()
   return _value;
 }
 
-void Digit::drawPixelFlip(uint16_t x, uint16_t y, Color c)
+void Digit::drawPixel(int16_t x, int16_t y, Color c)
 {
-  drawPixel(x, (2 * segHeight + 3) - 1 - y, c);
-}
-
-void Digit::drawPixel(uint16_t x, uint16_t y, Color c)
-{
-  _display->SetPixel(xOffset + x, y + yOffset, c.r, c.g, c.b);
-}
-
-void Digit::drawLineFlip(uint16_t x, uint16_t y, uint16_t x2, uint16_t y2, Color c)
-{
-  drawLine(x, (2 * segHeight + 3) - 1 - y, x2, (2 * segHeight + 3) - 1 - y2, c);
+  _display->SetPixel(xOffset + x, (2 * segHeight + 3) - 1 - y + yOffset, c.r, c.g, c.b);
 }
 
 void Digit::drawLine(uint16_t x, uint16_t y, uint16_t x2, uint16_t y2, Color c)
 {
-  DrawLine(_display, xOffset + x, y + yOffset, xOffset + x2, y2 + yOffset, c);
+  DrawLine(_display, xOffset + x, (2 * segHeight + 3) - 1 - y + yOffset, xOffset + x2, (2 * segHeight + 3) - 1 - y2 + yOffset, c);
 }
 
-void Digit::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, Color color)
+void Digit::drawFillRect(int16_t x, int16_t y, uint16_t w, uint16_t h, Color color)
 {
   for (int16_t x_pos = x; x_pos < x + w; x_pos++)
   {
-    for (int16_t y_pos = y; y_pos < y + h; y_pos++)
+    for (int16_t y_pos = y; y_pos > y - h; y_pos--)
     {
-      _display->SetPixel(x_pos, y_pos, color.r, color.g, color.b);
+      drawPixel(x_pos, y_pos, color);
     }
   }
-}
-
-void Digit::drawFillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, Color c)
-{
-  fillRect(xOffset + x, y + yOffset, w, h, c);
 }
 
 void Digit::DrawColon(Color c)
 {
   // Colon is drawn to the left of this digit
-  drawFillRect(-3, segHeight+1-3, 2, 2, c);
-  drawFillRect(-3, segHeight+3, 2, 2, c);
+  drawFillRect(-3, segHeight-1, 2,2, c);
+  drawFillRect(-3, segHeight+1+3, 2,2, c);
 }
 
 void Digit::drawSeg(char seg)
 {
   switch (seg) {
-    case sA: drawLine(1, 0, segWidth, 0, _color); break;
-    case sB: drawLine(segWidth + 1, 1, segWidth + 1, segHeight, _color); break;
-    case sC: drawLine(segWidth + 1, segHeight * 2 + 1, segWidth + 1, segHeight + 2, _color); break;
-    case sD: drawLine(1, segHeight * 2 + 2, segWidth, segHeight * 2 + 2, _color); break;
-    case sE: drawLine(0, segHeight * 2 + 1, 0, segHeight + 2, _color); break;
-    case sF: drawLine(0, 1, 0, segHeight, _color); break;
+    case sA: drawLine(1, segHeight * 2 + 2, segWidth, segHeight * 2 + 2, _color); break;
+    case sB: drawLine(segWidth + 1, segHeight * 2 + 1, segWidth + 1, segHeight + 2, _color); break;
+    case sC: drawLine(segWidth + 1, 1, segWidth + 1, segHeight, _color); break;
+    case sD: drawLine(1, 0, segWidth, 0, _color); break;
+    case sE: drawLine(0, 1, 0, segHeight, _color); break;
+    case sF: drawLine(0, segHeight * 2 + 1, 0, segHeight + 2, _color); break;
     case sG: drawLine(1, segHeight + 1, segWidth, segHeight + 1, _color); break;
   }
 }
@@ -128,13 +113,13 @@ void Digit::Morph2() {
   for (int i = 0; i <= segWidth; i++)
   {
     if (i < segWidth) {
-      drawPixelFlip(segWidth - i, segHeight * 2 + 2, _color);
-      drawPixelFlip(segWidth - i, segHeight + 1, _color);
-      drawPixelFlip(segWidth - i, 0, _color);
+      drawPixel(segWidth - i, segHeight * 2 + 2, _color);
+      drawPixel(segWidth - i, segHeight + 1, _color);
+      drawPixel(segWidth - i, 0, _color);
     }
 
-    drawLineFlip(segWidth + 1 - i, 1, segWidth + 1 - i, segHeight, black);
-    drawLineFlip(segWidth - i, 1, segWidth - i, segHeight, _color);
+    drawLine(segWidth + 1 - i, 1, segWidth + 1 - i, segHeight, black);
+    drawLine(segWidth - i, 1, segWidth - i, segHeight, _color);
     usleep(_animSpeed);
   }
 }
@@ -143,8 +128,8 @@ void Digit::Morph3() {
   // THREE
   for (int i = 0; i <= segWidth; i++)
   {
-    drawLineFlip(0 + i, 1, 0 + i, segHeight, black);
-    drawLineFlip(1 + i, 1, 1 + i, segHeight, _color);
+    drawLine(0 + i, 1, 0 + i, segHeight, black);
+    drawLine(1 + i, 1, 1 + i, segHeight, _color);
     usleep(_animSpeed);
   }
 }
@@ -153,9 +138,9 @@ void Digit::Morph4() {
   // FOUR
   for (int i = 0; i < segWidth; i++)
   {
-    drawPixelFlip(segWidth - i, segHeight * 2 + 2, black); // Erase A
-    drawPixelFlip(0, segHeight * 2 + 1 - i, _color); // Draw as F
-    drawPixelFlip(1 + i, 0, black); // Erase D
+    drawPixel(segWidth - i, segHeight * 2 + 2, black); // Erase A
+    drawPixel(0, segHeight * 2 + 1 - i, _color); // Draw as F
+    drawPixel(1 + i, 0, black); // Erase D
     usleep(_animSpeed);
   }
 }
@@ -164,9 +149,9 @@ void Digit::Morph5() {
   // FIVE
   for (int i = 0; i < segWidth; i++)
   {
-    drawPixelFlip(segWidth + 1, segHeight + 2 + i, black); // Erase B
-    drawPixelFlip(segWidth - i, segHeight * 2 + 2, _color); // Draw as A
-    drawPixelFlip(segWidth - i, 0, _color); // Draw D
+    drawPixel(segWidth + 1, segHeight + 2 + i, black); // Erase B
+    drawPixel(segWidth - i, segHeight * 2 + 2, _color); // Draw as A
+    drawPixel(segWidth - i, 0, _color); // Draw D
     usleep(_animSpeed);
   }
 }
@@ -176,8 +161,8 @@ void Digit::Morph6() {
   for (int i = 0; i <= segWidth; i++)
   {
     // Move C right to left
-    drawLineFlip(segWidth - i, 1, segWidth - i, segHeight, _color);
-    if (i > 0) drawLineFlip(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
+    drawLine(segWidth - i, 1, segWidth - i, segHeight, _color);
+    if (i > 0) drawLine(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
     usleep(_animSpeed);
   }
 }
@@ -187,16 +172,16 @@ void Digit::Morph7() {
   for (int i = 0; i <= (segWidth + 1); i++)
   {
     // Move E left to right
-    drawLineFlip(0 + i - 1, 1, 0 + i - 1, segHeight, black);
-    drawLineFlip(0 + i, 1, 0 + i, segHeight, _color);
+    drawLine(0 + i - 1, 1, 0 + i - 1, segHeight, black);
+    drawLine(0 + i, 1, 0 + i, segHeight, _color);
 
     // Move F left to right
-    drawLineFlip(0 + i - 1, segHeight * 2 + 1, 0 + i - 1, segHeight + 2, black);
-    drawLineFlip(0 + i, segHeight * 2 + 1, 0 + i, segHeight + 2, _color);
+    drawLine(0 + i - 1, segHeight * 2 + 1, 0 + i - 1, segHeight + 2, black);
+    drawLine(0 + i, segHeight * 2 + 1, 0 + i, segHeight + 2, _color);
 
     // Erase D and G gradually
-    drawPixelFlip(1 + i, 0, black); // D
-    drawPixelFlip(1 + i, segHeight + 1, black); // G
+    drawPixel(1 + i, 0, black); // D
+    drawPixel(1 + i, segHeight + 1, black); // G
     usleep(_animSpeed);
   }
 }
@@ -206,17 +191,17 @@ void Digit::Morph8() {
   for (int i = 0; i <= segWidth; i++)
   {
     // Move B right to left
-    drawLineFlip(segWidth - i, segHeight * 2 + 1, segWidth - i, segHeight + 2, _color);
-    if (i > 0) drawLineFlip(segWidth - i + 1, segHeight * 2 + 1, segWidth - i + 1, segHeight + 2, black);
+    drawLine(segWidth - i, segHeight * 2 + 1, segWidth - i, segHeight + 2, _color);
+    if (i > 0) drawLine(segWidth - i + 1, segHeight * 2 + 1, segWidth - i + 1, segHeight + 2, black);
 
     // Move C right to left
-    drawLineFlip(segWidth - i, 1, segWidth - i, segHeight, _color);
-    if (i > 0) drawLineFlip(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
+    drawLine(segWidth - i, 1, segWidth - i, segHeight, _color);
+    if (i > 0) drawLine(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
 
     // Gradually draw D and G
     if (i < segWidth) {
-      drawPixelFlip(segWidth - i, 0, _color); // D
-      drawPixelFlip(segWidth - i, segHeight + 1, _color); // G
+      drawPixel(segWidth - i, 0, _color); // D
+      drawPixel(segWidth - i, segHeight + 1, _color); // G
     }
     usleep(_animSpeed);
   }
@@ -227,8 +212,8 @@ void Digit::Morph9() {
   for (int i = 0; i <= (segWidth + 1); i++)
   {
     // Move E left to right
-    drawLineFlip(0 + i - 1, 1, 0 + i - 1, segHeight, black);
-    drawLineFlip(0 + i, 1, 0 + i, segHeight, _color);
+    drawLine(0 + i - 1, 1, 0 + i - 1, segHeight, black);
+    drawLine(0 + i, 1, 0 + i, segHeight, _color);
     usleep(_animSpeed);
   }
 }
@@ -239,49 +224,49 @@ void Digit::Morph0() {
   {
     if (_value==1) { // If 1 to 0, slide B to F and E to C  
       // slide B to F 
-      drawLineFlip(segWidth - i, segHeight * 2+1 , segWidth - i, segHeight + 2, _color);
-      if (i > 0) drawLineFlip(segWidth - i + 1, segHeight * 2+1, segWidth - i + 1, segHeight + 2, black);
+      drawLine(segWidth - i, segHeight * 2+1 , segWidth - i, segHeight + 2, _color);
+      if (i > 0) drawLine(segWidth - i + 1, segHeight * 2+1, segWidth - i + 1, segHeight + 2, black);
 
       // slide E to C
-      drawLineFlip(segWidth - i, 1, segWidth - i, segHeight, _color);
-      if (i > 0) drawLineFlip(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
+      drawLine(segWidth - i, 1, segWidth - i, segHeight, _color);
+      if (i > 0) drawLine(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
 
-      if (i<segWidth) drawPixelFlip(segWidth - i, segHeight * 2 + 2 , _color); // Draw A
-      if (i<segWidth) drawPixelFlip(segWidth - i, 0, _color); // Draw D
+      if (i<segWidth) drawPixel(segWidth - i, segHeight * 2 + 2 , _color); // Draw A
+      if (i<segWidth) drawPixel(segWidth - i, 0, _color); // Draw D
     }
     
     if (_value==2) { // If 2 to 0, slide B to F and Flow G to C
       // slide B to F 
-      drawLineFlip(segWidth - i, segHeight * 2+1 , segWidth - i, segHeight + 2, _color);
-      if (i > 0) drawLineFlip(segWidth - i + 1, segHeight * 2+1, segWidth - i + 1, segHeight + 2, black);
+      drawLine(segWidth - i, segHeight * 2+1 , segWidth - i, segHeight + 2, _color);
+      if (i > 0) drawLine(segWidth - i + 1, segHeight * 2+1, segWidth - i + 1, segHeight + 2, black);
     
-      drawPixelFlip(1+i, segHeight + 1, black); // Erase G left to right
-      if (i<segWidth) drawPixelFlip(segWidth + 1, segHeight + 1- i, _color);// Draw C
+      drawPixel(1+i, segHeight + 1, black); // Erase G left to right
+      if (i<segWidth) drawPixel(segWidth + 1, segHeight + 1- i, _color);// Draw C
     }
 
     if (_value==3) { // B to F, C to E
       // slide B to F 
-      drawLineFlip(segWidth - i, segHeight * 2+1 , segWidth - i, segHeight + 2, _color);
-      if (i > 0) drawLineFlip(segWidth - i + 1, segHeight * 2+1, segWidth - i + 1, segHeight + 2, black);
+      drawLine(segWidth - i, segHeight * 2+1 , segWidth - i, segHeight + 2, _color);
+      if (i > 0) drawLine(segWidth - i + 1, segHeight * 2+1, segWidth - i + 1, segHeight + 2, black);
       
       // Move C to E
-      drawLineFlip(segWidth - i, 1, segWidth - i, segHeight, _color);
-      if (i > 0) drawLineFlip(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
+      drawLine(segWidth - i, 1, segWidth - i, segHeight, _color);
+      if (i > 0) drawLine(segWidth - i + 1, 1, segWidth - i + 1, segHeight, black);
 
       // Erase G from right to left
-      drawPixelFlip(segWidth - i, segHeight + 1, black); // G
+      drawPixel(segWidth - i, segHeight + 1, black); // G
     }
     
     if (_value==5) { // If 5 to 0, we also need to slide F to B
       if (i<segWidth) {
-        if (i>0) drawLineFlip(1 + i, segHeight * 2 + 1, 1 + i, segHeight + 2, black);
-        drawLineFlip(2 + i, segHeight * 2 + 1, 2 + i, segHeight + 2, _color);
+        if (i>0) drawLine(1 + i, segHeight * 2 + 1, 1 + i, segHeight + 2, black);
+        drawLine(2 + i, segHeight * 2 + 1, 2 + i, segHeight + 2, _color);
       }
     }
     
     if (_value==5 || _value==9) { // If 9 or 5 to 0, Flow G into E
-      if (i<segWidth) drawPixelFlip(segWidth - i, segHeight + 1, black);
-      if (i<segWidth) drawPixelFlip(0, segHeight - i, _color);
+      if (i<segWidth) drawPixel(segWidth - i, segHeight + 1, black);
+      if (i<segWidth) drawPixel(0, segHeight - i, _color);
     }
     usleep(_animSpeed);
   }
@@ -292,17 +277,17 @@ void Digit::Morph1() {
   for (int i = 0; i <= (segWidth + 1); i++)
   {
     // Move E left to right
-    drawLineFlip(0 + i - 1, 1, 0 + i - 1, segHeight, black);
-    drawLineFlip(0 + i, 1, 0 + i, segHeight, _color);
+    drawLine(0 + i - 1, 1, 0 + i - 1, segHeight, black);
+    drawLine(0 + i, 1, 0 + i, segHeight, _color);
 
     // Move F left to right
-    drawLineFlip(0 + i - 1, segHeight * 2 + 1, 0 + i - 1, segHeight + 2, black);
-    drawLineFlip(0 + i, segHeight * 2 + 1, 0 + i, segHeight + 2, _color);
+    drawLine(0 + i - 1, segHeight * 2 + 1, 0 + i - 1, segHeight + 2, black);
+    drawLine(0 + i, segHeight * 2 + 1, 0 + i, segHeight + 2, _color);
 
     // Gradually Erase A, G, D
-    drawPixelFlip(1 + i, segHeight * 2 + 2, black); // A
-    drawPixelFlip(1 + i, 0, black); // D
-    drawPixelFlip(1 + i, segHeight + 1, black); // G
+    drawPixel(1 + i, segHeight * 2 + 2, black); // A
+    drawPixel(1 + i, 0, black); // D
+    drawPixel(1 + i, segHeight + 1, black); // G
 
     usleep(_animSpeed);
   }
